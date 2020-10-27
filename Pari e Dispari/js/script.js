615030f7d103f8659507077793834ce2b78690e0
Vanessa D'Alessandro <leavesthatfall@gmail.com>
2020-10-27 17:15:37 +0100

    fine js

--- Pari e Dispari/js/script.js
+++ Pari e Dispari/js/script.js
@@ -8,15 +8,39 @@
 //chiedo all'utente se punta pari o dispari
 var scommessa=prompt("Cosa scommetti? Scrivi pari o dispari");
 
-console.log(scommessa);
+console.log("hai scommesso "+scommessa);
 
 var numeroUtente=parseInt(prompt("Inserisci un numero da 1 a 5"));
 
-console.log(numeroUtente);
+console.log("hai giocato il numero "+numeroUtente);
 
 function pcRandomNumber(moltiplicatore){
    return Math.floor(Math.random() * moltiplicatore +1);
 };
 
 var pcNumber=pcRandomNumber(5);
-console.log(pcNumber)
+console.log("il pc ha giocato il numero "+pcNumber);
+
+// Sommiamo i due numeri
+var somma=numeroUtente+pcNumber;
+
+console.log(somma);
+
+//stabiliamo se la somma è pari o dispari
+function isItEvenOrOdd(number){
+    if (number % 2===0){
+    return "pari" } else {
+        return "dispari";
+    }
+}
+
+var pariDispari= isItEvenOrOdd(somma);
+console.log("la somma dei due numeri è "+pariDispari);
+
+
+//stabiliamo se l'utente ha vinto o perso
+if (scommessa===pariDispari){
+    console.log("Complimenti, hai vinto!");
+} else{
+    console.log("Mi dispiace, hai perso!");
+}
